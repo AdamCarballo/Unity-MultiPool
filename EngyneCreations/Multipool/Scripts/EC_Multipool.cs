32db234d614bb3b9ade38543635a6db207ad82fe
Adam Carballo <adam@engynecreations.com>
2016-11-01 22:14:44 +0100

    Fixed links file names and spelling, Added Rigidbody.velocity reset option, Fixed README.md, Improved demo scene, Created new Unitypackage.

--- EngyneCreations/Multipool/Scripts/EC_Multipool.cs
+++ EngyneCreations/Multipool/Scripts/EC_Multipool.cs
@@ -1,22 +1,27 @@
 ﻿/*
- * EC_ObjectPooling.cs
+ * EC_Multipool.cs
  * Manage the pooling of any object and calls to retrieve them.
  * 
  * by Adam Carballo under GPLv3 license.
- * https://github.com/engyne09/UnityObjectPooling
+ * https://github.com/AdamEC/Unity-MultipoolObjectPooling
  */
 
 using UnityEngine;
 using System.Collections.Generic;
 
-public class EC_ObjectPooling : MonoBehaviour {
+public class EC_Multipool : MonoBehaviour {
 
     [System.Serializable]
     public class Pool {
+        [Tooltip("Name of this pool as will appear on the dropdown of the emitters.")]
         public string name;
+        [Tooltip("Object used by this pool.")]
         public GameObject poolObject;
+        [Tooltip("How many objects will get pre-spawned before the scene starts (Pool size).")]
         public int startAmount;
+        [Tooltip("Can this pool grow if the requested items are higher than the start amount.")]
         public bool canGrow = true;
+        [Tooltip("Will this pool spawn objects inside a custom parent, or use the default one.")]
         public Transform customParent;
         [HideInInspector]
         public List<GameObject> poolList = new List<GameObject>();
@@ -24,16 +29,24 @@ public class EC_ObjectPooling : MonoBehaviour {
 
     public Pool[] pool;
 
-    static public EC_ObjectPooling instance;
+    static public EC_Multipool instance;
     static public List<string> generatedEnum = new List<string>();
 
-
-    // Define Reference
+    /// <summary>
+    /// Define Reference
+    /// </summary>
     void Awake() {
         instance = this;
+
+        Object[] otherPool= FindObjectsOfType(this.GetType());
+        if (otherPool.Length > 1) {
+            Debug.LogError("Multipool can only exist once in the scene.");
+        }
     }
 
-    // Instantiate the startAmount of poolObject and deactivate them.
+    /// <summary>
+    /// Instantiate the startAmount of poolObject and deactivate them.
+    /// </summary>
     void Start () {
 
         for (int i = 0; i < pool.Length; i++) {
@@ -50,15 +63,15 @@ public class EC_ObjectPooling : MonoBehaviour {
         }
 	}
 
-    // EDITOR function. Generates a list of all the object pools names to be shown on an enum.
+    /// <summary>
+    /// EDITOR function. Generates a list of all the object pools names to be shown on an enum.
+    /// </summary>
 #if UNITY_EDITOR
     public void GenerateEnum() {
 
         generatedEnum = new List<string>();
 
-        if (pool.Length == 0) {
-            return;
-        }
+        if (pool.Length == 0) return;
 
         for (int i = 0; i < pool.Length;i++) {
             generatedEnum.Add(pool[i].name);
@@ -66,7 +79,11 @@ public class EC_ObjectPooling : MonoBehaviour {
     }
 #endif
 
-    // Returns an availabe object. If there isn't one, and canGrow, will create a new one.
+    /// <summary>
+    /// Returns an availabe object. If there isn't one, and canGrow, will create a new one.
+    /// </summary>
+    /// <param name="index">Index of the pool.</param>
+    /// <returns></returns>
     public GameObject GetPooledObject(int index) {
 
         for (int i = 0; i < pool[index].poolList.Count;i++) {
---
+++ EngyneCreations/Multipool/Scripts/EC_MultipoolReset.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * EC_MultipoolReset.cs
+ * Demostration Script. Deactivates himself after time.
+ * 
+ * This script is optional. Just remember to deactivate objects to make them available again.
+ * Also, if your object uses physics, remember to reset velocity.
+ * 
+ * by Adam Carballo under GPLv3 license.
+ * https://github.com/AdamEC/Unity-MultipoolObjectPooling
+ */
+
+using UnityEngine;
+
+public class EC_MultipoolReset : MonoBehaviour {
+
+    public float time;
+    public bool resetRigidbody;
+
+    /// <summary>
+    /// Execute Destroy function after time.
+    /// </summary>
+	void OnEnable () {
+
+        Invoke("Destroy", time);
+	}
+
+    /// <summary>
+    /// Deactivate current gameObject.
+    /// </summary>
+    void Destroy() {
+
+        gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// Avoid multiple activations by canceling the Destroy invoke.
+    /// </summary>
+	void OnDisable () {
+
+        CancelInvoke();	
+	}
+}
